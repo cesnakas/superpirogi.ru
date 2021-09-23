1ecc4ec69d1a7dfabb76881221b821c33afd8419
Sergei ★ Česnakas <10352420+cesnakas@users.noreply.github.com>
2021-09-23 16:45:41 +0400

    Update links

---
+++ include/main_sections-links.php
@@ -0,0 +1,7 @@
+<a class="d-inline-block mb-2 btn btn-bordered-gold" href="/catalog/s-gribami/">с грибами</a>
+<a class="d-inline-block mb-2 btn btn-bordered-gold" href="/catalog/s-kapustoy/">с капустой</a>
+<a class="d-inline-block mb-2 btn btn-bordered-gold" href="/catalog/s-kartofelem/">с картофелем</a>
+<a class="d-inline-block mb-2 btn btn-bordered-gold" href="/catalog/s_kuritsey/">с курицей</a>
+<a class="d-inline-block mb-2 btn btn-bordered-gold" href="/catalog/s-myasom/">с мясом</a>
+<a class="d-inline-block mb-2 btn btn-bordered-gold" href="/catalog/s-ryboy/">с рыбой</a>
+<a class="d-inline-block mb-2 btn btn-bordered-gold" href="/catalog/s-syrom/">с сыром</a>
